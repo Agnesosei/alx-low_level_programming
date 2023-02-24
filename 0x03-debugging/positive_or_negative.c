909b2e5224dc78eb1cd9030751ad1b1d2db328e2
Agnesosei <agnesosei52@gmail.com>
2023-02-23 19:10:10 -0800

    Debugging Task

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * positive_or_negative - generate a random number
+ *@i : integer
+ * Return: Always 0 (Success)
+ *
+ */
+
+void positive_or_negative(int i)
+{
+
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+
+	}
+	else if (i < 0)
+	{
+		printf("%d is negative\n", i);
+	}
+	else
+	{
+		printf("%d is zero\n", i);
+	}
+}
