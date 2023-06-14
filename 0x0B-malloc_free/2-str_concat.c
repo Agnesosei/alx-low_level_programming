d7888304744ae6a9e682d6080e29220e00207ff5
Agnesosei <agnesosei52@gmail.com>
2023-06-13 22:09:43 -0700

    2nd commit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -25,7 +25,7 @@ char *str_concat(char *s1, char *s2)
 	for (i = 0; s1[i] || s2[i]; i++)
 		lent++;
 
-	jns = malloc(sizeof(char) * lent +1);
+	jns = malloc(sizeof(char) * lent);/*allocate memory*/
 
 	if (jns == NULL)
 		return (NULL);
