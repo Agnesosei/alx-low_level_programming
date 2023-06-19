b8b60d66c7bbe9bf2a4755761ae11e80f7bfe851
Agnesosei <agnesosei52@gmail.com>
2023-06-19 15:33:19 -0700

    The C preprocessor

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,11 @@
+#ifndef SUM_H
+#define SUM_H
+
+/*
+ * File: 4-sum.h
+ * Desc: Defines a function-like macro SUM(x, y)
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,2 @@
+ This project is on the C prepocessor
+An assignment by ALX Africa to test my C skills so far.
