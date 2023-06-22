b306736d3e44eda3b222c72bcac8bc85039f6ace
Agnesosei <agnesosei52@gmail.com>
2023-06-21 17:34:15 -0700

    2nd commit. Function pointers

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,73 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * op_add - addition operator
+ i* @a: first number
+ * @b: second number
+ *
+ * Return: sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - minus operator
+ * @a: first number
+ * @b: second number
+ *
+ * Return: difference of a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplication op
+ * @a: first number
+ * @b: second number
+ *
+ * Return: product of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: div of a and b
+ */
+int op_div(int a, int b)
+{
+	if (!b)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
+}
+
+/**
+ * op_mod - modulus of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: remainder of the division of a by b.
+ */
+int op_mod(int a, int b)
+{
+	if (!b)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
+}
